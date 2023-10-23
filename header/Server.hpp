d469b5cec3c81db769b681081b0788a39c232e4d
Atroooo <lucas.compiegne1@gmail.com>
2023-10-23 14:10:11 +0200

    organise server

--- header/Server.hpp
+++ header/Server.hpp
@@ -17,24 +17,18 @@ class Server {
         Server(list<struct pollfd> & fds);
         ~Server();
 
-        void setList( vector<struct pollfd> fds);
-
         list<struct pollfd>& getFdsList() const { return this->_fds; };
-        void addToFds(const struct pollfd& fd) {
-            cout << "add to fds: " << fd.fd << endl;
-            this->_fds.push_back(fd); };
-
         struct pollfd * getFd(size_t index);
-
-        void setServChanCount(size_t count) { this->_serverFdsCount = count; };
         size_t getServChanCount() const { return this->_serverFdsCount; };
-
-        void addClient(Client & client);
         list<Client> getClient(void) const;
         Client *getClient(string name);
         Client *getClient(int fd);
-
-        void addChannel(Channel & channel);
         list<Channel> getChannel(void) const;
         Channel *getChannel(string name);
+
+        void setList( vector<struct pollfd> fds);
+        void setServChanCount(size_t count) { this->_serverFdsCount = count; };
+        void addToFds(const struct pollfd& fd) { this->_fds.push_back(fd); };
+        void addClient(Client & client);
+        void addChannel(Channel & channel);
 };
