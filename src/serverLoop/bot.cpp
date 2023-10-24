b583a1a94baf33b1f393805aac1f5439389eccdd
Neoff69 <gnt.valentin@gmail.com>
2023-10-24 12:14:04 +0200

    add of bot

--- src/serverLoop/bot.cpp
+++ src/serverLoop/bot.cpp
@@ -2,6 +2,31 @@
 #include "../../header/Server.hpp"
 #include "../../header/typedef.hpp"
 
-void botAction(char *buf){
-	(void)buf;
+bool checkIfIn(char *buf, char *substr){
+    int i = 0;
+    while (strncmp(&buf[i], substr, 5) != 0 && buf[i]) {
+        i++;
+    }
+	if (strncmp(&buf[i], substr, 5) != 0){
+		return false;
+	}
+    return true;
+}
+
+
+void botAction(char *buf, int clientSocket, int x){
+	 if (x < 0) {
+        cerr << "Error in send(). Quitting" << endl;
+        //NEED EXIT
+    }
+	if (checkIfIn(buf, (char *)"quoi") || checkIfIn(buf, (char *)"Quoi")){
+		x = send(clientSocket, "quoicoubeh", sizeof(buf), 0);
+	}
+	else if (checkIfIn(buf, (char *)"hein") || checkIfIn(buf, (char *)"Hein")){
+		x = send(clientSocket, "apagnan", sizeof(buf), 0);
+	}
+	if (x < 0) {
+        cerr << "Error in send(). Quitting" << endl;
+        //NEED EXIT
+    }
 }
