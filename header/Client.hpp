7fe74aebb0b4f153fe76c70b4fca56314b38b7f7
Jules Laisne <jlaisne@z1r4p5.42lyon.fr>
2023-11-07 14:29:29 +0100

    MALAISE INTO LITTLE FIXES MY BAD TEAM

--- header/Client.hpp
+++ header/Client.hpp
@@ -14,6 +14,7 @@ class Client {
         string                      _username;
         bool                        _passCheck;
         list<string>                _cmdToSend;
+        string                      _buffer;
 
     public:
         Client(string name, int fd);
@@ -25,6 +26,8 @@ class Client {
         bool getPassCheck(void) const;
         list<string> getCmdToSend(void) const;
         string getUsername(void) const;
+        string& getBuffer() { return this->_buffer; };
+
 
         void setName(std::string name);
         void setFd(int fd);
@@ -32,4 +35,6 @@ class Client {
         void addCmdToSend(string cmd);
         void clearCmdToSend(void);
         void setPassCheck(bool passCheck);
+        void setBuffer(string buffer) { this->_buffer = buffer; };
+        void addToBuffer(string buffer) { this->_buffer += buffer; };
 };
